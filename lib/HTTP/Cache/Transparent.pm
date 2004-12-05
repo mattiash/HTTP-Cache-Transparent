f6445da6d8fa8bbc1388c49b5a470b871e7e9802
Mattias Holmlund <mattias.git@holmlund.se>
2004-12-05 11:41:03 +0000

    Handle spaces in cache-path.

--- lib/HTTP/Cache/Transparent.pm
+++ lib/HTTP/Cache/Transparent.pm
@@ -51,6 +51,7 @@ use HTTP::Status qw/RC_NOT_MODIFIED RC_OK RC_PARTIAL_CONTENT/;
 use Digest::MD5 qw/md5_hex/;
 use IO::File;
 use File::Copy;
+use Cwd;
 
 my $basepath;
 my $maxage;
@@ -171,7 +172,7 @@ sub simple_request_cache
     my $fh;
     my $meta;
 
-    if( -f $filename )
+    if( -s $filename )
     {
       $fh = new IO::File "< $filename"
         or die "Failed to read from $filename";
@@ -353,12 +354,15 @@ sub remove_old_entries
 {
   if( defined( $basepath ) )
   {
-    my @files = glob($basepath . "*");
+    my $oldcwd = getcwd();
+    chdir( $basepath );
+
+    my @files = glob("*");
     foreach my $file (@files)
     {
-      if( $file !~ m%/[0-9a-f]{32}$% )
+      if( $file !~ m%/^[0-9a-f]{32}$% )
       {
-        print STDERR "HTTP::Cache::Transparent: Unknown file found in cache directory: $file\n";
+        print STDERR "HTTP::Cache::Transparent: Unknown file found in cache directory: $basepath$file\n";
       }
       elsif( (-M($file))*24 > $maxage )
       {
@@ -367,6 +371,8 @@ sub remove_old_entries
         unlink( $file );
       }
     }
+
+    chdir( $oldcwd );
   }
 }
 
